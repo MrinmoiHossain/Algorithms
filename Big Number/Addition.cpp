16da2545d2d56973bafbab77b44e26344d4b1230
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-25 23:45:56 +0600

    algorithms

---
+++ Big Number/Addition.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int T;
+    cin >> T;
+
+    for(int i = 1; i <= T; i++){
+        string a, b;
+        cin >> a >> b;
+
+        cout << a << endl;
+        cout << b << endl;
+    }
+
+    return 0;
+}
