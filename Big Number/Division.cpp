910a29468592a3046bbf494e7983733461b46564
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-28 00:54:32 +0600

    algorithms

---
+++ Big Number/Division.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string division(string a, string b);
+
+int main(void)
+{
+    int T;
+    cin >> T;
+
+    for(int i = 1; i <= T; i++){
+        string a, b;
+        cin >> a >> b;
+
+        cout << division(a, b) << endl;
+    }
+
+    return 0;
+}
+
+string division(string a, string b)
+{
+    string s;
+
+    return s;
+}
