b93a8488445e890ae075557326790f58a03f490e
MrinmoiHossain <mokit.aust@gmail.com>
2017-05-28 15:08:37 +0600

    algorithms

--- Big Number/Division.cpp
+++ Big Number/Division.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
+#define LL long long int
 using namespace std;
 
-string division(string a, string b);
+string division(string a, LL b);
 
 int main(void)
 {
@@ -9,7 +10,8 @@ int main(void)
     cin >> T;
 
     for(int i = 1; i <= T; i++){
-        string a, b;
+        string a;
+        LL b;
         cin >> a >> b;
 
         cout << division(a, b) << endl;
@@ -18,9 +20,12 @@ int main(void)
     return 0;
 }
 
-string division(string a, string b)
+string division(string a, LL b)
 {
     string s;
+    for(int i = 0; i < s.length(); i++){
+
+    }
 
     return s;
 }
