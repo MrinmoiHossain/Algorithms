c00a40b4f487300b698e5eb7b3ccad1a90cb27b1
MrinmoiHossain <mokit.aust@gmail.com>
2017-09-30 00:17:29 +0600

    search, len, insert

--- Data Structure/Linked List/linkedList.cpp
+++ Data Structure/Linked List/linkedList.cpp
@@ -11,22 +11,6 @@ struct Node{
 struct Node *head, *tail;
 
 /*------Insert Function-------*/
-//Insetr into head	->O(1)
-void headInsert(int data)
-{
-	if(head == NULL){
-		head = (struct Node *)malloc(sizeof(struct Node));
-		head->data = data;
-		head->next = NULL;
-		tail = head;
-	}
-	else{
-		struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
-		temp->data = data;
-		temp->next = head;
-		head = temp;
-	}
-}
 //Inset into tail	->O(1)
 void tailInsert(int data)
 {
@@ -44,28 +28,61 @@ void tailInsert(int data)
 		tail = tail->next;
 	}
 }
-//Inset data after every n
-void InsertAfter(int data, int n)
+//Insetr into head	->O(1)
+void headInsert(int data)
 {
+	if(head == NULL){
+		head = (struct Node *)malloc(sizeof(struct Node));
+		head->data = data;
+		head->next = NULL;
+		tail = head;
+	}
+	else{
+		struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+		temp->data = data;
+		temp->next = head;
+		head = temp;
+	}
+}
 
+//Inset value after every n		->O(n)
+void insertAfter(int n, int value)
+{
+	struct Node *current, *temp;
+	if(head != NULL){
+		current = head;
+
+		while(current != NULL){
+			if(current->data == n){
+				temp = (struct Node *)malloc(sizeof(struct Node));
+				temp->data = value;
+				temp->next = current->next;
+				if(current->next == NULL)
+					tail = temp;
+				current->next = temp;
+				current = current->next;
+			}
+			current = current->next;
+		}
+	}
 }
 //Insert data before every n
-void InsetBefore(int data, int n)
+void insetBefore(int data, int n)
 {
 
 }
 
-//Length function -> O(n)
-int len(Node *head)
+//Length function -> O(1)
+int len()
 {
-	int con = 0;
-	Node *new_node = head;
-	while(new_node != NULL){
-		con++;
-		new_node = new_node->next;
-	}
+	//int con = 0;
+	//Node *new_node = head;
+	//while(new_node != NULL){
+		//con++;
+		//new_node = new_node->next;
+	//}
 
-	return con;
+	return abs(tail - head);
 }
 
 
@@ -91,12 +108,28 @@ void Delete(Node *head, int data)
 	}
 }
 
-//Print the Node value
+//Search into Linked List		->linear search: O(n)
+int searchLinkedList(int value)
+{
+	struct Node *current = head;
+	int pos = 0;
+	while(current != NULL){
+		if(current->data == value)
+			return pos;
+		pos++;
+		current = current->next;
+	}
+	return -1;						//if not found in the list
+}
+
+//Print the All Node value
 void Print()
 {
-	while(head != NULL){
-		cout << head->data << endl;
-		head = head->next;
+	struct Node *current = head;
+
+	while(current != NULL){
+		cout << current->data << endl;
+		current = current->next;
 	}
 }
 
@@ -111,6 +144,30 @@ int main(void)
 	for(int i = 0; i < 5; i++)
 		headInsert(i);
 	Print();
+
+	cout << "Length: " << len() << endl;
+	cout << endl;
+
+	for(int i = 1, n, v; i <= 3; i++){			//insert value after every n
+		cout << "Position value: ";
+		cin >> n;
+		cout << "Value: ";
+		cin >> v;
+
+		insertAfter(n, v);
+		Print();
+	}
+
+	cout << "---#Search#---" << endl;
+	for(int i = 0, n; i < 5; i++){
+		cin >> n;
+
+		int flag = searchLinkedList(n);			//0 indexing
+		if(flag == - 1)
+			cout << n << " not found" << endl;
+		else
+			cout << n << " found at " << flag << endl;
+	}
 
 	return 0;
 }
