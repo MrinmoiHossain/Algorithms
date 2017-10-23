61bd127b8a63cc4011f2b91cae0fe81208000126
MrinmoiHossain <mokit.aust@gmail.com>
2017-10-23 21:42:40 +0600

    queue

---
+++ Data Structure/Queue/Queue.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	queue<int> q;
+	for(int i = 1; i <= 5; i++)
+		q.push(i);
+
+	cout << "Size of queue: " << q.size() << endl;
+	cout << "The queue" << endl;
+	while(!q.empty()){
+		cout << q.front() << endl;
+		q.pop();
+	}
+
+	return 0;
+}
