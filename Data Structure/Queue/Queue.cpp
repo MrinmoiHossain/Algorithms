1463079d0fcaec06f2185e0f2c0cd4c20b1cbaef
MrinmoiHossain <mokit.aust@gmail.com>
2017-10-27 22:46:49 +0600

    queue back()

--- Data Structure/Queue/Queue.cpp
+++ Data Structure/Queue/Queue.cpp
@@ -8,6 +8,7 @@ int main(void)
 		q.push(i);
 
 	cout << "Size of queue: " << q.size() << endl;
+	cout << "Last value of queue: " << q.back() << endl;
 	cout << "The queue" << endl;
 	while(!q.empty()){
 		cout << q.front() << endl;
