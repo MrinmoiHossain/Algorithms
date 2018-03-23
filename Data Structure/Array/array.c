0ce2fad1f370704e00b666cca6d176fdcc859a7b
MrinmoiHossain <mokit.aust@gmail.com>
2018-03-23 21:01:49 +0600

    array-deleteFunction

--- Data Structure/Array/array.c
+++ Data Structure/Array/array.c
@@ -5,7 +5,9 @@
 //Declare Array ->data type will be: int, double, float, char
 int arr[MAX_SIZE];
 
-void ReadArrayValue(int n)
+int n;				//final array size
+
+void ReadArrayValue()
 {
 	int i = 0;
 	printf("Type the array value\n");
@@ -14,7 +16,7 @@ void ReadArrayValue(int n)
 	}
 }
 
-void PrintArray(int n)
+void PrintArray()
 {
 	int i = 0;
 	printf("The Array: ");
@@ -24,39 +26,62 @@ void PrintArray(int n)
 	printf("\n");
 }
 
-void UpdateArray(int m)
+void UpdateArray()
 {
 	printf("How many times you want to update array: ");
-	int n;
-	scanf("%d", &n);
+	int t;
+	scanf("%d", &t);
 
-	
-	printf("Position and Value\n");
+	if(t > 0)
+		printf("1-based: Position and Value\n");
 	int i = 0, pos, val;
-	for(i = 0; i < n; i++){
+	for(i = 0; i < t; i++){
 		scanf("%d %d", &pos, &val);
 		--pos;
 		
-		if(pos >= 0 && pos <= m)
+		if(pos >= 0 && pos <= n)
 			arr[pos] = val;
 		else
 			printf("Invalid position\n"); 		
 	}
 }
 
+void DeleteArray()
+{
+	printf("Delete Element\n");
+	printf("1-based: Position\n");
+	int index;
+	scanf("%d", &index);
+	
+	int i = --index;
+
+	if(index < 0 || index > n)
+		printf("Invalid position\n");
+	else{
+		
+		while(i < n - 1){
+			arr[i] = arr[i + 1];
+			i++;
+		}
+	}
+
+	n--;
+}
+
 int main(void)
 {
-    int n;
 	printf("Say the array size: ");
 	scanf("%d", &n);
-
 	arr[n];				//know as Array declaration
 
-	ReadArrayValue(n);	//know as Array Initialization
-	PrintArray(n);		//know as Array Traversing
+	ReadArrayValue();	//know as Array Initialization
+	PrintArray();		//know as Array Traversing
 
-	UpdateArray(n);		//know as Array Update
-	PrintArray(n);
+	UpdateArray();		//know as Array Update
+	PrintArray();
+
+	DeleteArray();		//know as Array Delete
+	PrintArray();
 
    return 0;
 }
